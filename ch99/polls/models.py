a1dfc4cffcf43b899ca0a4d5b9ab8c9ef3cd0a5f
[PHJ] <[mk26opo6@gmail.com]>
2023-09-27 09:57:16 +0900

    슈퍼유저 계정 생성

--- ch99/polls/models.py
+++ ch99/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    Question_test = models.CharField(max_length = 200)
+    pub_date = models.DateField("date_bulicshed")
+    def __str__(self):
+        return self.Question_text
+    
+class Choice(models.Model):
+    Question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    Choice_Text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.Choice_Text
