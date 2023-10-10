c446eb1c02084c86850630f7d5736733d35c3483
[PHJ] <[mk26opo6@gmail.com]>
2023-10-10 14:19:53 +0900

    장고 에서 id로 인수 전달

--- ch99/polls/views.py
+++ ch99/polls/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return HttpResponse('안녕하세요 polls index 홈페이지에 오신 것을 환영합니다.')
+
+def creat(request):
+    return HttpResponse('안녕하세요 polls creat 홈페이지에 오신 것을 환영합니다.')
+
+def read(request, id):
+    return HttpResponse('안녕하세요 polls read '+id+' 홈페이지에 오신 것을 환영합니다.')
